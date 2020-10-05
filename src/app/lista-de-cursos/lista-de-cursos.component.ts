b19424b26b29e83a80b2d210cc7c4597e414cd87
Ezequiel Perez <ezzeqquiel@hotmail.com>
2020-10-04 23:26:17 -0300

    Semana 5 - Introdução ao framework Angular

---
+++ src/app/lista-de-cursos/lista-de-cursos.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-cursos',
+  templateUrl: './lista-de-cursos.component.html',
+  styleUrls: ['./lista-de-cursos.component.css']
+})
+export class ListaDeCursosComponent {
+
+
+}
---
+++ src/app/lista-de-professores/lista-de-professores.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-professores',
+  templateUrl: './lista-de-professores.component.html',
+  styleUrls: ['./lista-de-professores.component.css']
+})
+export class ListaDeProfessoresComponent {
+
+
+}
